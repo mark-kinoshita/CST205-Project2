74bc5c295e962fb6cb112ec02ac111f2d5738441
Mark <mkinoshita@csumb.edu>
2016-10-13 14:50:08 -0700

    GUI

---
+++ CST205Project2/Gui.py
@@ -0,0 +1,23 @@
+import sys
+from os import listdir
+from os.path import isfile, join
+from pydub import AudioSegment
+from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit, QInputDialog, QApplication, QLabel)
+
+app = QApplication(sys.argv)
+text, ok = QInputDialog.getText(None, 'Project' , 'Please enter the location of your song library.  (Windows Ex:C:''\\\\''Users''\\\\''Users''\\\\''Desktop''\\\\''Library')
+
+path = text
+
+onlyfiles = [join(path,f) for f in listdir(path) if isfile(join(path,f)) ]
+for f in onlyfiles:
+
+  if f.endswith(".mp3" or ".m4a"):
+    print(f)    ## Lists the Filenames out as they are entered into converting.
+    onlyfiles = AudioSegment.from_mp3(f)
+    name = f.split(".")[0].split("\\")[-1] ## takes the name from the list to rename the 
+    print(path + "\\{}.wav".format(name))    ## Lists the filenames out after they have been converted.
+    onlyfiles.export (path + "\\Converted\\{}.wav".format(name), format="wav") ## outputs the files into the specified directory.
+
+  
+
